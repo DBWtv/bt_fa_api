90255cae19e78b32c9d6e852c4ca4a1277eea483
DBWtv <dima.bwc@gmail.com>
2023-09-29 10:49:56 +0600

    trying to fix csrf

--- user_messages/api/views.py
+++ user_messages/api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .services import validate, send
+# import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 class MessageView(APIView):
+    @csrf_exempt
     def post(self, request):
         
         send(*validate(request))
