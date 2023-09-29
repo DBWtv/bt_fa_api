307b8b54d60db1eda899d7d2ca7a9201aeec953c
DBWtv <dima.bwc@gmail.com>
2023-09-29 12:44:08 +0600

    del

--- CustomUser/views.py
+++ CustomUser/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponseBadRequest
 
 
 class HomeView(View):
@@ -18,8 +16,3 @@ def register(request):
     if request.user.is_authenticated:
         return redirect('/')
     return render(request, 'register.html')
-
-@csrf_exempt
-def get(request):
-    print(request.headers)
-    return HttpResponseBadRequest()
